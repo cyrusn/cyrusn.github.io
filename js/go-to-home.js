a0d7965af6058ef9643663c00f2fb10abcdbb17a
CyrusN <CyrusN@CyrusN.local>
2017-05-16 18:52:04 +0800

    add revealjs

---
+++ js/go-to-home.js
@@ -0,0 +1,15 @@
+// press 'h' key to go to homepage
+// console.log('loading keypress.js')
+document.onkeypress = function(e) {
+    e = e || window.event;
+    var charCode = (typeof e.which == "number") ? e.which : e.keyCode;
+    if (window.location.pathname == '/') return;
+
+    if (charCode == 104) {
+        window.location.href = '/'
+    }
+
+    if (charCode == 113) {
+        window.location.href = '/'
+    }
+};
