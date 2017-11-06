aefda2d3113f9db1340bac8e4469f760f57d7ca7
cyrusn <cyrusncy@gmail.com>
2017-11-06 23:38:07 +0800

    use notebook-searcher theme

--- js/search.js
+++ js/search.js
@@ -1,108 +1,73 @@
-var lunrIndex, pagesIndex;
+/* global $ fetch lunr _ */
+var pageIndex, lunrIndex
 
-function endsWith(str, suffix) {
-    return str.indexOf(suffix, str.length - suffix.length) !== -1;
+function initLunr (cb) {
+  fetch('/index.json')
+  .then((response) => {
+    return response.json()
+  })
+  .then(data => {
+    pageIndex = data
+    lunrIndex = lunr(function () {
+      this.ref('index')
+      this.field('title', {boost: 15})
+      this.field('tags', {boost: 10})
+      this.field('description', {boost: 5})
+      this.field('content', {boost: 5})
+
+      pageIndex.forEach(function (doc) {
+        this.add(doc)
+      }, this)
+
+      this.pipeline.remove(this.stemmer)
+    })
+  })
+  .then(cb)
 }
 
-// Initialize lunrjs using our generated index file
-function initLunr(cb) {
-    if (!endsWith(baseurl,"/")){
-        baseurl = baseurl+'/'
-    };
+initLunr(function () {
+  $('#search-box').focus()
+  listenQuery()
+})
 
-    // First retrieve the index file
-    $.getJSON(baseurl +"index.json")
-        .done(function(index) {
-            pagesIndex =   index;
-            // Set up lunrjs by declaring the fields we use
-            // Also provide their boost level for the ranking
-            lunrIndex = new lunr.Index
-            lunrIndex.ref("uri");
-            lunrIndex.field('title', {
-                boost: 15
-            });
-            lunrIndex.field('tags', {
-                boost: 10
-            });
-
-            lunrIndex.field("content", {
-                boost: 5
-            });
-
-            // Feed lunr with each file and let lunr actually index them
-            pagesIndex.forEach(function(page) {
-                lunrIndex.add(page);
-            });
-            lunrIndex.pipeline.remove(lunrIndex.stemmer)
-        })
-        .done(cb)
-        .fail(function(jqxhr, textStatus, error) {
-            var err = textStatus + ", " + error;
-            console.error("Error getting Hugo index flie:", err);
-        });
-
+function search (query) {
+  return lunrIndex.search(query)
+  .map(result => {
+    return Object.assign({
+      score: result.score
+    }, pageIndex[result.ref], {content: ''})
+    
+  })
+  .filter(a => a.score > 0.2)
 }
 
-/**
- * Trigger a search in lunr and transform the result
- *
- * @param  {String} query
- * @return {Array}  results
- */
-function search(query) {
-    // Find the item in our index corresponding to the lunr one to have more info
-    return lunrIndex.search(query).map(function(result) {
-            return pagesIndex.filter(function(page) {
-                return page.uri === result.ref;
-            })[0];
-        });
+function listenQuery () {
+  $('#search-box').on('input keydown focus', function () {
+    const query = $(this).val().toLowerCase()
+    const results = search(query)
+    displaySearchResults(results, pageIndex)
+  })
 }
 
-// Let's get started
-initLunr(function() {
-  document.getElementById("search-by").focus();
-});
-$( document ).ready(function() {
-    var horseyList = horsey($("#search-by").get(0), {
-        suggestions: function (value, done) {
-            var query = $("#search-by").val();
-            var results = search(query);
-            done(results);
-        },
-          filter: function (q, suggestion) {
-              return true;
-          },
-        set: function (value) {
-            location.href = value.uri;
-        },
-        render: function (li, suggestion) {
-            var uri = suggestion.uri.substring(1,suggestion.uri.length);
+function displaySearchResults (results, pageIndex) {
+  var listItems = results.map(({index, date, uri, title, tags}) => {
+    var badges = tags.map(word => `<a href="/tags/${word}" class='badge badge-pill badge-secondary'>${word}</a>`).join(' ')
 
-            suggestion.href = baseurl + uri;
+    return `<div 
+      id='${index}'
+      class="list-group-item list-group-item-action list-group-item-light">
+      <div class="d-flex w-100 justify-content-between">
+        <h5 class="mb-1"><a class="text-muted" href="${uri}">${title}</a></h5>
+        <small>${date}</small>
+      </div>
+      <p class='mb-0'>
+        <a class="text-muted" href="${uri}"> ${uri.replace(window.location.origin, '')} </a>
+      </p>
+      <small>
+        ${badges}
+      </small>
+    </div>`
+  }).join('')
 
-            var query = $("#search-by").val().toLowerCase();
-
-            // suggestion.context = text;
-
-            var image = '<h4 style="margin: 0;">' + suggestion.title + '</h4>'
-            var linkIcon = '<span class="glyphicon glyphicon-link"></span> '
-            var parser = document.createElement('a')
-            parser.href = suggestion.uri
-            image += '<div style="font-size:12px">' + linkIcon +  (parser.pathname || '') +'</div>';
-
-            if (suggestion.content) {
-              var numWords = 3;
-              var text = suggestion.content.toLowerCase().match("(?:\\s?(?:[\\w]+)\\s?){0,"+numWords+"}"+query+"(?:\\s?(?:[\\w]+)\\s?){0,"+numWords+"}");
-            }
-
-            if (text) {
-              var contentIcon = '<span class="glyphicon glyphicon-file"></span> '
-              image += '<div style="font-size:12px; font-style: italic"> ' + contentIcon + '... ' + (text || '') + ' ...' + '</div>';
-            }
-
-            li.innerHTML = image;
-        },
-        highlighter: true
-    });
-    horseyList.refreshPosition();
-});
+  $('#search-result').html(`<div class="list-group">${listItems}</div>`)
+}
